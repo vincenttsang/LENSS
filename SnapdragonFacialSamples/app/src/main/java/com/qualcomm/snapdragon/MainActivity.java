454d154248f96c2c6c74e8463940d9836e9481ea
Vincent Tsang <yukun.zeng.hit@outlook.com>
2016-12-03 01:06:29 -0600

    now face recognition is good to go, still need to test if features work fine on snapdragon devices

---
+++ SnapdragonFacialSamples/app/src/main/java/com/qualcomm/snapdragon/MainActivity.java
@@ -0,0 +1,44 @@
+package com.qualcomm.snapdragon;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.qualcomm.snapdragon.sdk.recognition.sample.FacialRecognitionActivity;
+import com.qualcomm.snapdragon.sdk.sample.CameraPreviewActivity;
+import com.qualcomm.snapdragon.sdk.sample.R;
+
+/**
+ * Created by yukun on 12/1/2016.
+ */
+
+public class MainActivity extends Activity {
+    public final static String EXTRA_MSG = "com.qualcomm.snapdragon.sdk.sample.MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        Button facialProcBtn = (Button) this.findViewById(R.id.button_processing);
+        facialProcBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CameraPreviewActivity.class);
+                intent.putExtra(EXTRA_MSG, "call facial proc sample");
+                startActivity(intent);
+            }
+        });
+
+        Button facialRecoBtn = (Button) this.findViewById(R.id.btn_recog);
+        facialRecoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FacialRecognitionActivity.class);
+                intent.putExtra(EXTRA_MSG, "call facial recog sample");
+                startActivity(intent);
+            }
+        });
+    }
+}
