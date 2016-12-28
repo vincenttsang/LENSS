5525a7ec0fc051a42f9dffe67b795a00b2d91a11
Vincent Tsang <yukun.zeng.hit@outlook.com>
2016-12-27 23:12:11 -0600

    added code for spout to collect camera frame data and emit to downstream components TODO add face detector as processor in the downstream to finish face detection and feedback to main activity

---
+++ SnapdragonFacialSamples/app/src/main/java/com/lenss/yzeng/FaceDetector.java
@@ -0,0 +1,11 @@
+package com.lenss.yzeng;
+
+import com.lenss.qning.greporter.topology.Processor;
+
+/**
+ * Created by yukun on 12/18/2016.
+ */
+
+public class FaceDetector extends Processor {
+
+}
